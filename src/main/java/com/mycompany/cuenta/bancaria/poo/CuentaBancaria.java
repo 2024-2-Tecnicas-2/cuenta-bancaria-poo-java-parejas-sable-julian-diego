5ab20be713c3aaa1d81df0d1d70d3ba2f1953be5
Diego Lares <diegoalares33@gmail.com>
2024-09-06 21:27:25 -0500

    Actualizacion1

--- src/main/java/com/mycompany/cuenta/bancaria/poo/CuentaBancaria.java
+++ src/main/java/com/mycompany/cuenta/bancaria/poo/CuentaBancaria.java
@@ -1,5 +1,36 @@
 package com.mycompany.cuenta.bancaria.poo;
 
 public class CuentaBancaria {
-    // TODO: Aquí va el código de la clase CuentaBancaria
+
+    private String titular;
+    private String numeroCuenta;
+    private double saldo;
+    private double tipoInteres;
+
+    public CuentaBancaria(String titular, String numeroCuenta, double saldo, double tipoInteres) {
+        this.titular = titular;
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+        this.tipoInteres = 0.15;
+    }
+    
+  
+    
+
+    public double ingresar(double cantidad) {
+
+        return 0;
+    }
+
+    public double retirar(double cantidad) {
+        return 0;
+    }
+    public void calcularInteres(){
+    
+    }
+    
+    public double setTipoInteres(double tipoInteres){
+    
+    return 0;
+    }
 }
--- src/main/java/com/mycompany/cuenta/bancaria/poo/Principal.java
+++ src/main/java/com/mycompany/cuenta/bancaria/poo/Principal.java
@@ -1,7 +1,50 @@
 package com.mycompany.cuenta.bancaria.poo;
 
+import java.util.Scanner;
+
 public class Principal {
+
     public static void main(String[] args) {
-        // TODO: Adiciona aquí el código que deseas para la Cuenta Bancaria.
+
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        //Creamos Objetos (Titualares de cuenta)
+        CuentaBancaria cuenta1 = new CuentaBancaria("Diego", "128341243253", 222200, 0);
+        CuentaBancaria cuenta2 = new CuentaBancaria("Julian", "1283412353", 2343450, 0);
+        CuentaBancaria cuenta3 = new CuentaBancaria("Santiago", "1283452341233", 223432200, 0);
+        CuentaBancaria cuenta4 = new CuentaBancaria("Maradona", "128345234453", 222202140, 0);
+
+        while (true) {
+            System.out.println("Ingrese la aopcion que desea gestionar: \n"
+                    + "1. Ver cuentas Bancarias \n"
+                    + "2. Calcular el interes de todas las cuentas \n"
+                    + "3. Cambiar el tipo de interes \n"
+                    + "4. Cambiar saldo \n"
+                    + "5. Salir");
+
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    
+                    break;
+
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+
+                case 5:
+                    System.out.println("Saliendo...");
+                    return;
+             }
+             System.out.println();
+        }
+
     }
 }
