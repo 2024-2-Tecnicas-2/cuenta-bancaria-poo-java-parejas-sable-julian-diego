f2ed98986b964e4d9876ba9e50d267d85653135b
giogonza21 <giovannygonzalez806@gmail.com>
2024-09-07 14:19:54 -0500

    Cambios en el main y también en el cuentaBancaria. Se realizo la tarea de agregar cuenta.

--- src/main/java/com/mycompany/cuenta/bancaria/poo/CuentaBancaria.java
+++ src/main/java/com/mycompany/cuenta/bancaria/poo/CuentaBancaria.java
@@ -1,67 +1,59 @@
 package com.mycompany.cuenta.bancaria.poo;
-
+ 
+import java.util.ArrayList;
+ 
 public class CuentaBancaria {
-
+ 
     private String titular;
     private String numeroCuenta;
     private double saldo;
     private double tipoInteres;
+   ArrayList<CuentaBancaria> cuenta = new ArrayList<>();
 
+ 
     public CuentaBancaria(String titular, String numeroCuenta, double saldo, double tipoInteres) {
         this.titular = titular;
         this.numeroCuenta = numeroCuenta;
         this.saldo = saldo;
         this.tipoInteres = 0.15;
     }
-    
-        
-     public String getTitular() {
+ 
+    public String getTitular() {
         return titular;
     }
-
-    public  void setTitular(String titular) {
+ 
+    public void setTitular(String titular) {
         this.titular = titular;
     }
-
+ 
     public String getNumeroCuenta() {
         return numeroCuenta;
     }
-
+ 
     public void setNumeroCuenta(String numeroCuenta) {
         this.numeroCuenta = numeroCuenta;
     }
-
+ 
     public double getSaldo() {
         return saldo;
     }
-
+ 
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
-
+ 
     public double getTipoInteres() {
         return tipoInteres;
     }
-
+ 
     public void setTipoInteres(double tipoInteres) {
         this.tipoInteres = tipoInteres;
     }
-
-    public double ingresar(double cantidad) {
-
-        return 0;
+ 
+    public void AgregarCuenta(CuentaBancaria v) {
+        cuenta.add(v);
     }
-
-    public double retirar(double cantidad) {
-        return 0;
+    public void mostrarCuentas(){
+        System.out.println("nombre titular: "+this.titular+" El número de cuenta es: "+this.cuenta+" El saldo de la cuenta es: "+this.saldo+" el tipo de interes es: "+this.tipoInteres);
     }
-    public void calcularInteres(){
-    
-    }
-
-   
-    
-   
-    
-    
 }
--- src/main/java/com/mycompany/cuenta/bancaria/poo/Principal.java
+++ src/main/java/com/mycompany/cuenta/bancaria/poo/Principal.java
@@ -1,52 +1,10 @@
 package com.mycompany.cuenta.bancaria.poo;
-
-import java.util.Scanner;
-
+ 
 public class Principal {
-
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        int opcion;
-        //Creamos Objetos (Titualares de cuenta)
-        CuentaBancaria cuenta1 = new CuentaBancaria("Diego", "128341243253", 222200, 0);
-        CuentaBancaria cuenta2 = new CuentaBancaria("Julian", "1283412353", 2343450, 0);
-        CuentaBancaria cuenta3 = new CuentaBancaria("Santiago", "1283452341233", 223432200, 0);
-        CuentaBancaria cuenta4 = new CuentaBancaria("Maradona", "128345234453", 222202140, 0);
-        
-        Principal opciones = new Principal();
-        while (true) {
-            System.out.println("Ingrese la aopcion que desea gestionar: \n"
-                    + "1. Agregar cuentas bancarias \n"
-                    + "1. Ver cuentas Bancarias \n"
-                    + "2. Calcular el interes de todas las cuentas \n"
-                    + "3. Cambiar el tipo de interes \n"
-                    + "4. Cambiar saldo \n"
-                    + "5. Salir");
-
-            opcion = sc.nextInt();
-
-            switch (opcion) {
-                case 1:
-                    System.out.println("");
-                    break;
-
-                case 2:
-
-                    break;
-                case 3:
-
-                    break;
-                case 4:
-
-                    break;
-
-                case 5:
-                    System.out.println("Saliendo...");
-                    return;
-             }
-             System.out.println();
-        }
-
+        CuentaBancaria cuenta1= new CuentaBancaria("dIEGO", "124521", 2453,0);
+        cuenta1.AgregarCuenta(cuenta1);
+        cuenta1.mostrarCuentas();
+        System.out.println("La cuenta es: ");
     }
 }
